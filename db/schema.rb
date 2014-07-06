81e37fe06cd10c83c0b943726359f0ef9f3776e9
Kyle Brown <brownkylej@gmail.com>
2014-07-06 00:09:16 -0500

    Migrated DB for Teacher

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705203658) do
+ActiveRecord::Schema.define(version: 20140706044847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20140705203658) do
     t.string   "state"
     t.string   "zip_code"
     t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teachers", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
